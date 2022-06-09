4621ecc7bf589d8c161cb20314695098e668ce5c
proux <proux03@outlook.com>
2022-06-09 20:21:39 +0800

    First commit, project started

---
+++ src/main/java/com/example/springboot/KataSpringBootModule3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSpringBootModule3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataSpringBootModule3Application.class, args);
+    }
+
+}
